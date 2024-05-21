4dc8196465ae1d507554af11d47661b1d988f99e
Tomasz Chmiel <tomasz.chmiel2@edu.uekat.pl>
2024-05-21 17:47:27 +0200

    checkpoint

---
+++ consumer.py
@@ -0,0 +1,103 @@
+import json
+import logging
+import urllib.request
+
+import cv2
+import os
+
+import numpy as np
+import pika
+
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_PORT = 5672
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT))
+channel = connection.channel()
+
+queue_name = 'chmiel_kolejka'
+channel.queue_declare(queue=queue_name)
+channel.basic_qos(prefetch_count=1)
+
+def draw_rectangles(image, detected_objects):
+    for i in range(detected_objects.shape[2]):
+        confidence = detected_objects[0][0][i][2]
+        if confidence > 0.3:
+            class_index = int(detected_objects[0, 0, i, 1])
+            if class_index == 15:
+                height, width = image.shape[0], image.shape[1]
+
+                upper_left_x = int(detected_objects[0, 0, i, 3] * width)
+                upper_left_y = int(detected_objects[0, 0, i, 4] * height)
+                lower_right_x = int(detected_objects[0, 0, i, 5] * width)
+                lower_right_y = int(detected_objects[0, 0, i, 6] * height)
+
+                cv2.rectangle(image, (upper_left_x, upper_left_y), (lower_right_x, lower_right_y), (0, 255, 0), 3)
+
+def count_people(detected_objects):
+    num_people = sum(1 for i in range(detected_objects.shape[2]) if int(detected_objects[0, 0, i, 1]) == 15)
+    return num_people
+
+def process_image(image_path):
+    prototxt_path = 'model/MobileNetSSD_deploy.prototxt.txt'
+    model_path = 'model/MobileNetSSD_deploy.caffemodel'
+
+    net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+
+    image = cv2.imread(image_path)
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007, (300, 300), 130)
+
+    net.setInput(blob)
+    detected_objects = net.forward()
+
+    draw_rectangles(image, detected_objects)
+
+    output_image_path = os.path.join("uploaded_images", "marked_" + os.path.basename(image_path))
+    cv2.imwrite(output_image_path, image)
+
+    num_people = count_people(detected_objects)
+
+    return num_people, output_image_path
+
+def read_image_from_url(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+def callback(ch, method, properties, body):
+    try:
+        task_data = json.loads(body.decode())
+        task_id = task_data['url_id']
+        url = task_data['url']
+        file_extension = task_data['file_extension']
+
+        image = read_image_from_url(url)
+
+        count, image_with_rects = 0, image
+        output_folder = 'unknown_output'
+
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        filename = f"{task_id}-{count}.{file_extension}"
+        output_path = os.path.join(output_folder, filename)
+
+        cv2.imwrite(output_path, image_with_rects)
+
+        message = f"Processed task with ID {task_id} by process {os.getpid()}. Detected {count} people. Image saved as {filename}. WELL DONE!"
+        logging.info(message)
+
+    except Exception as error:
+        error_message = f"Error processing task: {str(error)}"
+        logging.error(error_message)
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+
+def start_consuming():
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    start_consuming()
--- producer.py
+++ producer.py
@@ -1,16 +1,21 @@
 import logging
 import os
-
+import uuid
+import re
 from http.client import HTTPException
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import JSONResponse, HTMLResponse
 import requests
 from fastapi.templating import Jinja2Templates
 import shutil
-from counter import process_image
+
+from pika import channel
+
+from consumer import process_image
 import pika
 import aio_pika
 import json
+import subprocess
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -24,19 +29,35 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_PORT = 5672
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT))
+channel = connection.channel()
+
+queue_name = 'chmiel_kolejka'
+channel.queue_declare(queue=queue_name)
+
+consumer_processes = []
+
 
 async def get_rabbitmq_connection():
     return await aio_pika.connect_robust("amqp://guest:guest@localhost/")
 
+def clean_url(url):
+    cleaned_url = url.strip()
+    cleaned_url = re.sub(r'[\r\n]', '', cleaned_url)
+    return cleaned_url
 
-async def send_to_queue(message: dict):
-    connection = await get_rabbitmq_connection()
-    async with connection:
-        channel = await connection.channel()
-        await channel.default_exchange.publish(
-            aio_pika.Message(body=json.dumps(message).encode()),
-            routing_key="task_queue",
-        )
+def send_url_to_queue(url):
+    url_id = str(uuid.uuid4())
+    file_extension = url.split('.')[-1]
+    message_body = json.dumps({'task_id': url_id, 'url': url, 'file_extension': file_extension})
+
+    channel.basic_publish(exchange='', routing_key=queue_name, body=message_body)
+
+    message = f" [x] Sent URL: {url}  with ID {url_id} to RabbitMQ"
+    print(message)
 
 
 @app.get("/upload/", response_class=HTMLResponse)
@@ -78,35 +99,38 @@ async def detect_people_from_urls(request: Request):
     try:
         logger.info("Starting URL detection")
         form_data = await request.form()
-        image_urls = form_data["image_urls"].split("\n")  # Dziel linki po nowych liniach
-
-        upload_folder = "uploaded_images"
-        os.makedirs(upload_folder, exist_ok=True)
+        image_urls = form_data["image_urls"]
+        valid_urls = []
 
         for image_url in image_urls:
-            image_url = image_url.strip()  # Usuń białe znaki z linku
+            cleaned_url = clean_url(image_url)
+            print(cleaned_url)
             response = requests.get(image_url)
+            valid_urls.append(cleaned_url)
             if response.status_code != 200:
                 logger.error(f"Cannot download image from URL: {image_url}")
-                continue  # Przejdź do następnego URL, jeśli ten nie działa
+                continue
 
-            # Zapisz obraz do folderu "uploaded_images"
-            image_filename = os.path.basename(image_url)
-            image_path = os.path.join(upload_folder, image_filename)
-            with open(image_path, "wb") as img_file:
-                img_file.write(response.content)
+            send_url_to_queue(cleaned_url)
 
-            # Loguj ścieżkę do pobranego pliku
-            logger.info(f"Image downloaded and saved to: {image_path}")
+            logger.info(f"Image URL sent to RabbitMQ queue: {image_url}")
+            #wyślij kazdy link na kolejke rabbitmq
+            # # Zapisz obraz do folderu "uploaded_images"
+            # image_filename = os.path.basename(image_url)
+            # image_path = os.path.join(upload_folder, image_filename)
+            # with open(image_path, "wb") as img_file:
+            #     img_file.write(response.content)
+            #
+            # # Loguj ścieżkę do pobranego pliku
+            # logger.info(f"Image downloaded and saved to: {image_path}")
+            #
+            # try:
+            #     num_people, output_image_path = process_image(image_path)
+            #     logger.info(f"Image processed: {image_path}, people detected: {num_people}")
+            #     os.remove(image_path)
+            # except Exception as e:
+            #     logger.error(f"Error processing image {image_path}: {str(e)}")
 
-            try:
-                num_people, output_image_path = process_image(image_path)
-                logger.info(f"Image processed: {image_path}, people detected: {num_people}")
-                os.remove(image_path)
-            except Exception as e:
-                logger.error(f"Error processing image {image_path}: {str(e)}")
-
-            # Możesz dodać kod do wysłania wyników do klienta, jeśli to konieczne
 
         return {"message": f"{len(image_urls)} URL(s) processed"}
     except Exception as e:
